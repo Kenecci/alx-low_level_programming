585752702b74bbbe3a33d9bcd72bd453f20ad379
Kenecci <Ekenogbue@gmail.com>
2022-12-14 08:40:01 -0800

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -41,12 +41,6 @@ void print_times_table(int n)
 					_putchar('0' + ((product / 10) % 10));
 					_putchar('0' + (product % 10));
 				}
-				else if (product >= 100)
-				{
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
-				}
 			}
 		}
 		_putchar('\n');
