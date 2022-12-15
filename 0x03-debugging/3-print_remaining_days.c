4a948b05c976ee2304f9cad8631a8c727852ef7c
Kenecci <Ekenogbue@gmail.com>
2022-12-15 06:51:41 -0800

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,11 +20,12 @@ int print_remaining_days(int month, int day, int year)
 		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
+	}
 		else
 		{
 			if (month == 2 && day == 60)
 			{
-				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+				printf("Invalid date: %02d/%02d/%04d\n", 
 			}
 			else
 			{
@@ -32,4 +33,4 @@ int print_remaining_days(int month, int day, int year)
 				printf("Remaining days: %d\n", 365 - day);
 			}
 		}
-	}
+		}		
