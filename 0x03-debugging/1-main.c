6447719f15e3d6714766c7b3b302e2fce9f8884a
Kenecci <Ekenogbue@gmail.com>
2022-12-15 05:03:48 -0800

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Causes an infinte loop
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	int = 0;
+
+	/*
+	 * while (i < 10)
+	 * {
+	 * _putchar(i);
+	 * }
+	 */
+
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
