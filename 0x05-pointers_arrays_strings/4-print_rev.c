716151b995c271ac6485d1b6b3144da089e8678d
Kenecci <Ekenogbue@gmail.com>
2022-12-31 03:23:18 -0800

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_rev - prints a string, in reverse, followed by a new line
@@ -28,5 +29,5 @@ void print_rev(char *s)
 		s--;
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
